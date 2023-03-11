a077b935f4d913057da039d5c1a713e095a37898
sopiokvantaliani <skvantaliani@gmail.com>
2023-03-11 09:24:07 -0500

    Finalize Employee list and Registration form

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,7 +1,10 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @NoArgsConstructor //because we need one empty object to use in Form (html)
@@ -9,16 +12,41 @@ import java.time.LocalDate;
 @AllArgsConstructor //maybe we need it
 public class Employee {
 
-    private String firstName;
-    private String lastName;
-    private LocalDate birthday;
-    private String email;
-    private String password;
-    private String address;
-    private String address2;
-    private String city;
-    private String state;
-    private String zipCode;
+//    @NotNull        --->    Field shouldn't be null
+//    @NotEmpty       --->    Field shouldn't be ""
+//    @NotBlank       --->    Field shouldn't be "       "
+
+//    @NotNull -> @NotNull
+//    @NotEmpty -> @NotNull + @NotEmpty
+//    @NotBlank -> @NotNull + @NotEmpty + @NotBlank
+
+        @NotBlank
+        @Size(max = 12, min = 2)
+        private String firstName;
+
+        //    @NotBlank
+//    @Size(max = 12, min = 2)
+        private String lastName;
+
+        // Thymeleaf accepts yyyy-MM-dd, but LocalDate accepts mm-dd-yyyy  -   yyyy-dd-mm
+
+        //    @NotNull
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private LocalDate birthday;
+
+        //    @NotBlank
+//    @Email
+        private String email;
+
+        //    @NotBlank
+//    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
+        private String password;
+
+        private String address;
+        private String address2;
+        private String city;
+        private String state;
+        private String zipCode;
 
 
-}
+    }
